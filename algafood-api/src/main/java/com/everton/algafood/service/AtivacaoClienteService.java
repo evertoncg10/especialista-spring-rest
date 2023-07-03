9993ff1190dab2cd2b71aa50df7990dbcda15511
Everton Cezar Gonçalves <evertoncg10@outlook.com>
2023-07-03 16:05:19 -0300

    2.20. Mudando o comportamento da aplicação com Spring Profiles

--- algafood-api/src/main/java/com/everton/algafood/service/AtivacaoClienteService.java
+++ algafood-api/src/main/java/com/everton/algafood/service/AtivacaoClienteService.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class AtivacaoClienteService {
 
-	@TipoDoNotificador(NivelUrgencia.URGENTE)
+	@TipoDoNotificador(NivelUrgencia.NAO_URGENTE)
 	@Autowired
 	private Notificador notificador;
 
