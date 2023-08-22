ac984bbd2ddf8ea8b5dd23b0ad2d1eeaeb1d9276
Everton Cezar Gonçalves <evertoncg10@outlook.com>
2023-08-22 19:55:48 -0300

    3.10. Buscando um objeto pelo id no banco de dados

--- algafood-api/src/main/java/com/everton/algafood/jpa/CadastroCozinha.java
+++ algafood-api/src/main/java/com/everton/algafood/jpa/CadastroCozinha.java
@@ -18,8 +18,13 @@ public class CadastroCozinha {
                 .getResultList();
     }
 
+    public Cozinha buscar(Long id) {
+        return manager.find(Cozinha.class, id);
+    }
+
     @Transactional
     public Cozinha adicionar(Cozinha cozinha) {
         return manager.merge(cozinha);
     }
+
 }
