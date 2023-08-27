4c334d19ba80c0dd7c97bd659590685fcb6e832e
Everton Cezar Gonçalves <evertoncg10@outlook.com>
2023-08-27 14:03:54 -0300

    4.13. Implementando content negotiation para retornar JSON ou XML.

--- algafood-api/src/main/java/com/everton/algafood/api/controller/CozinhaController.java
+++ algafood-api/src/main/java/com/everton/algafood/api/controller/CozinhaController.java
@@ -3,6 +3,7 @@ package com.everton.algafood.api.controller;
 import com.everton.algafood.domain.model.Cozinha;
 import com.everton.algafood.domain.repository.CozinhaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,14 +11,21 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/cozinhas")
+@RequestMapping("/cozinhas") //, produces = MediaType.APPLICATION_JSON_VALUE
 public class CozinhaController {
 
     @Autowired
     private CozinhaRepository cozinhaRepository;
 
-    @GetMapping
-    public List<Cozinha> listar() {
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Cozinha> listar1() {
+        System.out.println("LISTAR 1");
+        return cozinhaRepository.listar();
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
+    public List<Cozinha> listar2() {
+        System.out.println("LISTAR 2");
         return cozinhaRepository.listar();
     }
 }
