22b76afbdbd23d8d56041b5a9bcdc2b4c097f7ec
Everton Cezar Gonçalves <evertoncg10@outlook.com>
2023-09-02 21:04:57 -0300

    4.20. Manipulando a resposta HTTP com ResponseEntity.

--- algafood-api/src/main/java/com/everton/algafood/api/controller/CozinhaController.java
+++ algafood-api/src/main/java/com/everton/algafood/api/controller/CozinhaController.java
@@ -3,7 +3,8 @@ package com.everton.algafood.api.controller;
 import com.everton.algafood.domain.model.Cozinha;
 import com.everton.algafood.domain.repository.CozinhaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,7 +25,10 @@ public class CozinhaController {
     }
 
     @GetMapping("/{cozinhaId}")
-    public Cozinha buscar(@PathVariable Long cozinhaId) {
-        return cozinhaRepository.buscar(cozinhaId);
+    public ResponseEntity<Cozinha> buscar(@PathVariable Long cozinhaId) {
+        var cozinha = cozinhaRepository.buscar(cozinhaId);
+
+//        return ResponseEntity.status(HttpStatus.OK).body(cozinha);
+        return ResponseEntity.ok(cozinha);
     }
 }
