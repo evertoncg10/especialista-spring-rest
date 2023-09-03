f24ba30010da498fadc3fc183cef3244df7b83b3
Everton Cezar Gonçalves <evertoncg10@outlook.com>
2023-09-03 14:46:39 -0300

    4.25. Modelando e implementando a atualização de recursos com PUT.

--- algafood-api/src/main/java/com/everton/algafood/api/controller/CozinhaController.java
+++ algafood-api/src/main/java/com/everton/algafood/api/controller/CozinhaController.java
@@ -2,6 +2,7 @@ package com.everton.algafood.api.controller;
 
 import com.everton.algafood.domain.model.Cozinha;
 import com.everton.algafood.domain.repository.CozinhaRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -34,5 +35,17 @@ public class CozinhaController {
     @ResponseStatus(HttpStatus.CREATED)
     public Cozinha adicionar(@RequestBody Cozinha cozinha) {
         return cozinhaRepository.salvar(cozinha);
+    }
+
+    @PutMapping("/{cozinhaId}")
+    public ResponseEntity<Cozinha> atualizar(@PathVariable Long cozinhaId, @RequestBody Cozinha cozinha) {
+        Cozinha cozinhaAtual = cozinhaRepository.buscar(cozinhaId);
+        if(cozinhaAtual != null) {
+            BeanUtils.copyProperties(cozinha, cozinhaAtual, "id");
+            cozinhaRepository.salvar(cozinhaAtual);
+            return ResponseEntity.ok(cozinhaAtual);
+
+        }
+        return ResponseEntity.notFound().build();
     }
 }
