9ff057dc2da6c2966927e8f010c50de110938b4d
Everton Cezar Gonçalves <evertoncg10@outlook.com>
2023-09-08 21:01:25 -0300

    4.30. Modelando e implementando a inclusão de recursos de restaurantes.

---
+++ algafood-api/src/main/java/com/everton/algafood/domain/service/CadastroRestauranteService.java
@@ -0,0 +1,32 @@
+package com.everton.algafood.domain.service;
+
+import com.everton.algafood.domain.exception.EntidadeNaoEncontradaException;
+import com.everton.algafood.domain.model.Cozinha;
+import com.everton.algafood.domain.model.Restaurante;
+import com.everton.algafood.domain.repository.CozinhaRepository;
+import com.everton.algafood.domain.repository.RestauranteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CadastroRestauranteService {
+
+    @Autowired
+    private RestauranteRepository restauranteRepository;
+
+    @Autowired
+    private CozinhaRepository cozinhaRepository;
+
+    public Restaurante salvar(Restaurante restaurante) {
+        Long cozinhaId = restaurante.getCozinha().getId();
+        Cozinha cozinha = cozinhaRepository.buscar(cozinhaId);
+
+        if (cozinha == null) {
+            throw new EntidadeNaoEncontradaException(
+                    String.format("Não existe cadastro de cozinha com o código %d", cozinhaId)
+            );
+        }
+        restaurante.setCozinha(cozinha);
+        return restauranteRepository.salvar(restaurante);
+    }
+}
