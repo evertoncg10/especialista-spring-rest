4e5cdf6505dd09f34558041afafdb1614d116047
Everton Cezar Gonçalves <evertoncg10@outlook.com>
2023-09-30 20:57:25 -0300

    5.9. Usando queries JPQL customizadas com @Query.

--- algafood-api/src/main/java/com/everton/algafood/api/controller/TesteController.java
+++ algafood-api/src/main/java/com/everton/algafood/api/controller/TesteController.java
@@ -44,7 +44,7 @@ public class TesteController {
 
     @GetMapping("/restaurantes/por-nome")
     public List<Restaurante> restaurantesPorNomeECozinhaId(String nome, Long cozinhaId) {
-        return restauranteRepository.findByNomeContainingAndCozinhaId(nome, cozinhaId);
+        return restauranteRepository.consultaPorNome(nome, cozinhaId);
     }
     @GetMapping("/restaurantes/primeiro-por-nome")
     public Optional<Restaurante> restaurantePrimeiroPorNome(String nome) {
--- algafood-api/src/main/java/com/everton/algafood/domain/repository/RestauranteRepository.java
+++ algafood-api/src/main/java/com/everton/algafood/domain/repository/RestauranteRepository.java
@@ -2,6 +2,8 @@ package com.everton.algafood.domain.repository;
 
 import com.everton.algafood.domain.model.Restaurante;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.math.BigDecimal;
 import java.util.List;
@@ -11,7 +13,10 @@ public interface RestauranteRepository extends JpaRepository<Restaurante, Long> 
 
     List<Restaurante> queryByTaxaFreteBetween(BigDecimal taxaInicial, BigDecimal taxaFinal);
 
-    List<Restaurante> findByNomeContainingAndCozinhaId(String nome, Long cozinhaId);
+    @Query("from Restaurante where nome like %:nome% and cozinha.id = :id")
+    List<Restaurante> consultaPorNome(String nome, @Param("id") Long cozinhaId);
+
+//    List<Restaurante> findByNomeContainingAndCozinhaId(String nome, Long cozinhaId);
 
     Optional<Restaurante> findFirstRestauranteByNomeContaining(String nome);
 
