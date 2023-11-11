dbfeab150603da0d44e15643173fd420f4224506
Everton Cezar Gonçalves <evertoncg10@outlook.com>
2023-11-11 13:22:15 -0300

    5.19. Injetando o próprio repositório na implementação customizada e a anotação @Lazy.

--- algafood-api/src/main/java/com/everton/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
+++ algafood-api/src/main/java/com/everton/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -1,7 +1,10 @@
 package com.everton.algafood.infrastructure.repository;
 
 import com.everton.algafood.domain.model.Restaurante;
+import com.everton.algafood.domain.repository.RestauranteRepository;
 import com.everton.algafood.domain.repository.RestauranteRepositoryQueries;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.StringUtils;
 
@@ -12,10 +15,16 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.everton.algafood.infrastructure.repository.spec.RestauranteSpecs.comFreteGratis;
+import static com.everton.algafood.infrastructure.repository.spec.RestauranteSpecs.comNomeSemelhante;
+
 @Repository
 public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries {
     @PersistenceContext
     private EntityManager manager;
+
+    @Autowired @Lazy
+    private RestauranteRepository restauranteRepository;
 
     @Override
     public List<Restaurante> find(String nome, BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal) {
@@ -43,5 +52,10 @@ public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries {
 
         return manager.createQuery(criteria)
                 .getResultList();
+    }
+
+    @Override
+    public List<Restaurante> findComFreteGratis(String nome) {
+        return restauranteRepository.findAll(comFreteGratis().and(comNomeSemelhante(nome)));
     }
 }
